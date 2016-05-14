53cd330cd4ea3667a4b02c4005b3dae7d522af72
Lee Azzarello <lee@seriesdigital.com>
2016-05-14 16:50:37 -0700

    initialization variables

---
+++ main.js
@@ -0,0 +1,14 @@
+// infinity plus one
+// scroll from zero to infinity on any webpage
+
+var bartop = document.body.scrollTop();
+var height = document.body.scrollHeight();
+var xstep,ystep = (0,50); // this doesn't really do what I think it does
+var position = bartop;
+
+//body.offsetHeight()
+//body.offsetWidth()
+//body.clientHeight()
+
+
+
