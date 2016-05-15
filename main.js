2efe41e86c5648cbadf4b207b34111c30e1ce336
Lee Azzarello <lee@seriesdigital.com>
2016-05-14 17:05:39 -0700

    easter egg by id

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ var xstep,ystep = (0,50); // this doesn't really do what I think it does
 var position = bartop;
 var sleep_val = 500;
 var gold_middle_finger = 63667;
+var gold_middle_finger_div = '<li class="post" id="post_122371942349">'
 
 //body.offsetHeight()
 //body.offsetWidth()
