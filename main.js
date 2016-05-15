5d877ac31a7da20e200a04b3580f84bb58eabd4d
Lee Azzarello <lee@seriesdigital.com>
2016-05-14 18:24:01 -0700

    I don't think I understand what is going on anymore

--- main.js
+++ main.js
@@ -3,38 +3,32 @@
 
 // initialize some settings
 var position = document.body.scrollTop;
-var height = document.body.scrollHeight;
 var xstep,ystep = (0,50); // this doesn't really do what I think it does
 var next_step = position + ystep;
-var sleep_val = 500;
+var sleep_val = 5000;
+
+// don't really need these
+var height = document.body.scrollHeight;
 var gold_middle_finger = 63667;
 var gold_middle_finger_div = '<li class="post" id="post_122371942349">';
 var test_url = 'http://thelivest1.com/';
 
 // recursive function to keep scrolling to the next step after sleeping for sleep_value between steps
-function scrollingTo(next_step) {
+console.log("current position is: " + position);
+function scrollingTo() {
+  console.log("next step is: " + next_step);
   document.body.scrollTo(0,next_step);
   position = document.body.scrollTop;
+  console.log("current position is: " + position);
   next_step = position + ystep;
-  setTimeout(scrollingTo(next_step), sleep_val);
+  setTimeout(scrollingTo(), sleep_val);
 }
 
-// or like this
-
-//while (true) {
-  // scroll stuff
-//  sleep(sleep_val);
-//}
-
-// or use setTimeout(function, sleep_val);
-//
-// start scrolling function
-var startScroll = function() {
-  scrollingTo(next_step);
+function init() {
+  scrollingTo();
 }
 
-// invoke the function to start scrolling
-startScroll();
+window.onload = init;
 
 //body.offsetHeight()
 //body.offsetWidth()
