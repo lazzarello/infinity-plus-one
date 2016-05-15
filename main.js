7373b676172a3df5054267f35f7ee63d8dfe3aed
Lee Azzarello <lee@seriesdigital.com>
2016-05-14 20:38:18 -0700

    uhhh, logic is hard

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ var position = document.body.scrollTop;
 var xstep,ystep = (0,50); // this doesn't really do what I think it does
 var next_step = position + ystep;
 var sleep_val = 5000;
+var height = document.body.scrollHeight;
 
 // don't really need these
-var height = document.body.scrollHeight;
 var gold_middle_finger = 63667;
 var gold_middle_finger_div = '<li class="post" id="post_122371942349">';
 var test_url = 'http://thelivest1.com/';
@@ -18,7 +18,8 @@ var test_url = 'http://thelivest1.com/';
 // recursive function to keep scrolling to the next step after sleeping for sleep_value between steps
 console.log("current position is: " + position);
 function scrollingTo() {
-  if ( position >= height ) {
+  console.log("current positions is: " + position);
+  if ( position <= height ) {
     console.log("next step is: " + next_step);
     document.body.scrollTo(0,next_step);
     position = document.body.scrollTop;
@@ -33,6 +34,7 @@ function bottom() {
 }
 
 function init() {
+  console.log("position is: " + position);
   scrollingTo();
 }
 
