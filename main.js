2fe544a251e1081e4a03a68b96038069ea5a3dcf
Lee Azzarello <lee@seriesdigital.com>
2016-05-14 17:02:20 -0700

    initialize a sleep value

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ var bartop = document.body.scrollTop();
 var height = document.body.scrollHeight();
 var xstep,ystep = (0,50); // this doesn't really do what I think it does
 var position = bartop;
+var sleep_val = 500;
 
 //body.offsetHeight()
 //body.offsetWidth()
