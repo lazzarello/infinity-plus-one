f995e48bd9ff5184d5a20912b034e1e95cc50b5a
Lee Azzarello <lee@seriesdigital.com>
2016-05-14 17:07:31 -0700

    initialize a next step, and some easter eggs

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 // infinity plus one
 // scroll from zero to infinity on any webpage
 
-var bartop = document.body.scrollTop();
-var height = document.body.scrollHeight();
+// initialize some settings
+var bartop = document.body.scrollTop;
+var height = document.body.scrollHeight;
 var xstep,ystep = (0,50); // this doesn't really do what I think it does
 var position = bartop;
+var next_step = position + ystep;
 var sleep_val = 500;
 var gold_middle_finger = 63667;
-var gold_middle_finger_div = '<li class="post" id="post_122371942349">'
+var gold_middle_finger_div = '<li class="post" id="post_122371942349">';
+var test_url = 'http://thelivest1.com/';
 
 //body.offsetHeight()
 //body.offsetWidth()
