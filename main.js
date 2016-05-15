75fc0b18bad5bb83b30641de1ef25f89a228a3f6
Lee Azzarello <lee@seriesdigital.com>
2016-05-14 21:37:34 -0700

    context, right. we are working with recursion

--- main.js
+++ main.js
@@ -4,11 +4,8 @@
 // https://stackoverflow.com/questions/17540458/stack-overflow-error-in-javascript-when-recursively-scrolling-to-the-bottom-of-t
 //
 // initialize some settings
-var position = document.body.scrollTop;
 var xstep,ystep = (0,50); // this doesn't really do what I think it does
-var next_step = position + ystep;
 var sleep_val = 5000;
-var height = document.body.scrollHeight;
 
 // don't really need these
 var gold_middle_finger = 63667;
@@ -16,13 +13,17 @@ var gold_middle_finger_div = '<li class="post" id="post_122371942349">';
 var test_url = 'http://thelivest1.com/';
 
 // recursive function to keep scrolling to the next step after sleeping for sleep_value between steps
-console.log("current position is: " + position);
+//console.log("current position is: " + position);
 function scrollingTo() {
   console.log("current height is: " + height);
   if ( position <= height ) {
+    var height = document.body.scrollHeight;
+    var position = document.body.scrollTop;
+    var next_step = position + ystep;
+
     console.log("next step is: " + next_step);
     document.body.scrollTo(0,next_step);
-    console.log("scrolled to next step: " + next);
+    console.log("scrolled to next step: " + next_step);
     position = document.body.scrollTop;
     console.log("current position is: " + position);
     next_step = position + ystep;
@@ -30,8 +31,8 @@ function scrollingTo() {
   }
 }
 
-function bottom() {
-  // the non-recursive version
+var bottom = function() {
+  scrollTo(0, next_step);
 }
 
 function init() {
