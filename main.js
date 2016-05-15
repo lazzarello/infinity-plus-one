72f387a5ef0d8207a9972653bb2f5e989221c1f5
Lee Azzarello <lee@seriesdigital.com>
2016-05-14 20:22:50 -0700

    first try with limits, check the height

--- main.js
+++ main.js
@@ -16,12 +16,18 @@ var test_url = 'http://thelivest1.com/';
 // recursive function to keep scrolling to the next step after sleeping for sleep_value between steps
 console.log("current position is: " + position);
 function scrollingTo() {
-  console.log("next step is: " + next_step);
-  document.body.scrollTo(0,next_step);
-  position = document.body.scrollTop;
-  console.log("current position is: " + position);
-  next_step = position + ystep;
-  setTimeout(scrollingTo(), sleep_val);
+  if ( position >= height ) {
+    console.log("next step is: " + next_step);
+    document.body.scrollTo(0,next_step);
+    position = document.body.scrollTop;
+    console.log("current position is: " + position);
+    next_step = position + ystep;
+    setTimeout(scrollingTo(), sleep_val);
+  }
+}
+
+function bottom() {
+  // the non-recursive version
 }
 
 function init() {
